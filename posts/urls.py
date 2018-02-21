7b5cb007ef9e1173072dd0a8bd1d67e5aaa5ce1b
hickedby <tufekcih16@itu.edu.tr>
2018-02-21 22:03:58 +0300

    slm

---
+++ posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import PostListView, PostDetailView
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='post_list'),
+    path('<int:pk>', PostDetailView.as_view(), name='post_detail'),
+
+ ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.models import User
 from django.shortcuts import HttpResponseRedirect
 from .models import Post, Comment
+
 
 class PostListView(ListView):
     queryset = Post.objects.all()
@@ -21,7 +22,21 @@ class PostListView(ListView):
         else:
             return HttpResponseRedirect('/login')
 
-    class PostDetailView(DetailView):
-        queryset = Post.objects.all()
-        template_name = 'posts/detail.html'
-        pk_url_kwarg = 'pk'
+
+class PostDetailView(DetailView):
+    queryset = Post.objects.all()
+    template_name = 'posts/detail.html'
+    pk_url_kwarg = 'pk'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(PostDetailView, self).get_context_data(*args, **kwargs)
+        context['comments'] = self.get_object().comment_set.all()
+        context['user'] = self.request.user
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super(PostDetailView, self).get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect('/login')
